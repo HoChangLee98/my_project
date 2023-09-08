b9670db2f3c4d7c098bb3003b17309a61fbe1551
HoChangLee98 <ghckd3558@nvaer.com>
2023-09-08 18:08:42 +0900

    Refactor : Refactor code error

--- competition_new_drug_development/src/feature_engineering.py
+++ competition_new_drug_development/src/feature_engineering.py
@@ -9,8 +9,8 @@ class FeatureEngineer:
         self.X_test = X_test
 
     def drop_cat_feature(self):
-        X_train_drop = self.X_train.drop(colums="SMILES")
-        X_test_drop = self.X_test.drop(colums="SMILES")
+        X_train_drop = self.X_train.drop(columns="SMILES")
+        X_test_drop = self.X_test.drop(columns="SMILES")
 
         return X_train_drop, X_test_drop
         
